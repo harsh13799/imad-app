490b11f6f2cfe7c3cfdd87ceefa01f6700500fb6
Harsh.13799 <37097060+harsh13799@users.noreply.github.com>
2018-04-16 20:47:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/My Image.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'My Image.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
